5b10a4c111ec547abd208d0bf8d6edb02fa499e8
sabadoryo <eabilbay@gmail.com>
2023-05-18 02:57:33 +0600

    fixes

--- src/bot.ts
+++ src/bot.ts
@@ -1,6 +1,6 @@
 import TelegramBot from 'node-telegram-bot-api';
 
-const token = process.env.TELEGRAM_BOT_TOKEN || "";
+const token = process.env.TELEGRAM_BOT_TOKEN ?? "";
 
 const bot = new TelegramBot(token, {polling: false});
 
