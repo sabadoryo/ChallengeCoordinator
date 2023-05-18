e7d3c6c65668aefca4f3a8d526685b1c908444c7
sabadoryo <eabilbay@gmail.com>
2023-05-18 23:07:48 +0600

    added games json

--- src/scheduler.ts
+++ src/scheduler.ts
@@ -6,7 +6,7 @@ import { getDay } from "./helpers";
 const dayModel = new Day();
 
 export const initScheduledJobs = () => {
-  const scheduledJobFunction = CronJob.schedule("*/10 * 23 * * *", async () => {
+  const scheduledJobFunction = CronJob.schedule("0 10 23 * * *", async () => {
     const dayData = await dayModel.getDayByNumber(getDay());
     
     const header = `День ${getDay()}.\n`
