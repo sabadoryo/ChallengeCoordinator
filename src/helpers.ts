fb302956fba5eafb11a7464a611c859eb42d8b89
sabadoryo <eabilbay@gmail.com>
2023-05-21 18:49:52 +0600

    holy shit huge bug found

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,6 +1,6 @@
 export const getDay = function() {
     const curDate = new Date();
     const startDate = new Date(process.env.START_DATE ?? "05 18 2023");
-    const diffTime = Math.abs(curDate.getMilliseconds() - startDate.getMilliseconds());
-    return Math.ceil(diffTime / (1000 * 60 * 60 * 24)) + 1;
+    const diffTime = Math.abs(curDate.valueOf() - startDate.valueOf());
+    return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 }
